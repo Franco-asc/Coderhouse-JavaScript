f9ab2f464e2fa26e8a427743e5b5d197721bfded
Franco Galavano <francogalvano@hotmail.com>
2022-07-20 01:32:08 -0300

    formulario de compra

--- script.js
+++ script.js
@@ -1,52 +1,66 @@
-// ALERTA DE BIENVENIDA 
+//Segunda Entrge Proyecto Final
 
-alert("Bienvenido a la inscripción virtual de Basquet del Club Atlantic Sportmen de Rosario");
-
-// INSCRIPCION 
-
-class alumno {
-    constructor(nombre, apellido, edad, categoria) {
-        this.nombre = nombre
-        this.apellido = apellido
-        this.edad = edad
-        this.categoria = categoria
+//Creamos un clas para la generacion de objetos
+class Articulo {
+    constructor (producto, precio, stock) {
+        this.producto = producto;
+        this.precio = precio;
+        this.stock = stock;
     }
 }
-const alumnos = []
 
-const form = document.getElementById("#idForm")
-const divAlumnos = document.getElementById('#divAlumnos')
+//Declaracion de base de datos
+let base_datos = [];
 
-form.addEventListener('submit' , (e) => {
-    e.preventDefault()
-    let nombre = document.getElementById('idNombre').value
-    let apellido = document.getElementById('idApellido').value
-    let edad = parseFloat(document.getElementById('idEdad').value) 
-    let categoria = document.getElementById('idCategoria').value
+//Consulta y creacion de localStorage
+if(localStorage.getItem('base_datos_articulos')){
+    base_datos = JSON.parse(localStorage.getItem('base_datos_articulos'))
+}
+else {
+    localStorage.setItem('base_datos_articulos', JSON.stringify(base_datos))
+}
 
-    const alumno = new alumno(nombre, apellido, edad, categoria)
+//Declaracion de constantes y llamado a nodos del html
+const boton = document.getElementById('formulario_carga')
+const div_producto = document.getElementById('productos')
+
+//Accion del formulario
+boton.addEventListener ('submit', (e) => {  
     
-    alumno.push(alumnos)
-    console.log(alumnos)
+    e.preventDefault()      
 
-    form.reset()
-})
+    let nombre_art = document.getElementById('nombre_producto').value;
+    let precio_art = parseFloat(document.getElementById('precio_producto').value);
+    let stock_art = parseInt(document.getElementById('stock_producto').value);
 
-document.querySelector('#botonAlumnos').addEventListener('click' , () =>{
-    alumnos.forEach(alumno => {
-        divAlumnos.innerHTML += `
-        <div class="card" style="width: 18rem;">
-            <div class="card-body">
-                <h5 class="card-title">Alumno Inscripto</h5>
-                <p class="card-text">${alumno.nombre}</p>
-                <p class="card-text">${alumno.apellido}</p>
-                <p class="card-text">${alumno.edad}</p>
-                <p class="card-text">${alumno.categoria}</p>
-                <button class="btn btn-danger">Desincribirse</button>
-            </div>
-        </div>
-        `
+    const producto1 = new Articulo (nombre_art, precio_art, stock_art);
+
+    base_datos.push(producto1)
+
+    localStorage.setItem('base_datos_articulos', JSON.stringify(base_datos))
+
+    div_producto.innerHTML = " "
+
+    formulario_carga.reset()
+
+    base_datos.forEach((producto,indice) => {
+        div_producto.innerHTML +=   `<div class="div_articulo" id="producto${indice}">
+                                        <h2>${producto.producto}</h2>
+                                        <p>${producto.precio}</p>
+                                        <p>${producto.stock}</p>
+                                        <input type="button" value="Eliminar" id="eliminar">
+                                    </div>`
+    })
+
+    base_datos.forEach((producto, indice) => {
+            document.getElementById(`producto${indice}`).lastElementChild.addEventListener('click', () => {
+            document.getElementById(`producto${indice}`).remove()
+            base_datos.splice(indice, 1)
+            localStorage.setItem('base_datos_articulos', JSON.stringify(base_datos))
+            alert(`${producto.producto} eliminado`)
+        })
     })
 })
 
+//--------------------------------------------------------------------------------------------------------------
 
